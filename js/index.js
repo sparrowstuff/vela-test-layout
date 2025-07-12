34f009305850060d16d283f19494ec5ed42d7ac3
sparrowstuff <damnagett@gmail.com>
2025-07-12 20:00:59 +0300

    added events menu
    JS opens events menu

--- js/index.js
+++ js/index.js
@@ -1,49 +1,106 @@
 document.addEventListener('DOMContentLoaded', function () {
-	const menus = [
-		document.getElementById('phonesMenu'),
-		document.getElementById('pcMenu'),
-		document.getElementById('tvMenu'),
-		document.getElementById('audioMenu'),
-	]
-
-	function showMenu(menuToShow) {
-		menus.forEach(menu => menu.classList.remove('choice-menu__show'))
+	// функция вывода меню через массив
+	function showMenu(array, menuToShow) {
+		array.forEach(menu => menu.classList.remove('choice-menu__show'))
 		menuToShow.classList.add('choice-menu__show')
 	}
 
-	const phoneChoice = document.getElementById('choicePhones')
-	const pcChoice = document.getElementById('choicePc')
-	const tvChoice = document.getElementById('choiceTv')
-	const audioChoice = document.getElementById('choiceAudio')
+	// управление бургер меню
+	const burgerBtn = document.getElementById('navigationBtn')
+	const mobileBurgerBtn = document.getElementById('navigationBtnMobile')
 
-	phoneChoice.addEventListener('click', () => {
-		showMenu(menus[0])
-	})
+	function burgerMenuActions() {
+		const menus = [
+			document.getElementById('phonesMenu'),
+			document.getElementById('pcMenu'),
+			document.getElementById('tvMenu'),
+			document.getElementById('audioMenu'),
+		]
 
-	pcChoice.addEventListener('click', () => {
-		showMenu(menus[1])
-	})
+		const phoneChoice = document.getElementById('choicePhones')
+		const pcChoice = document.getElementById('choicePc')
+		const tvChoice = document.getElementById('choiceTv')
+		const audioChoice = document.getElementById('choiceAudio')
 
-	tvChoice.addEventListener('click', () => {
-		showMenu(menus[2])
-	})
+		phoneChoice.addEventListener('click', () => {
+			showMenu(menus, menus[0])
+		})
 
-	audioChoice.addEventListener('click', () => {
-		showMenu(menus[3])
-	})
+		pcChoice.addEventListener('click', () => {
+			showMenu(menus, menus[1])
+		})
 
-	const burgerBtn = document.getElementById('navigation-btn')
-	const mobileBurgerBtn = document.getElementById('navigation-btn-mobile')
+		tvChoice.addEventListener('click', () => {
+			showMenu(menus, menus[2])
+		})
 
-	burgerBtn.addEventListener('click', () => {
-		burgerBtn.classList.toggle('navigation-btn--transform')
-		const choiceMenu = document.querySelector('.choice-menu')
-		choiceMenu.classList.toggle('choice-menu--show')
-	})
+		audioChoice.addEventListener('click', () => {
+			showMenu(menus, menus[3])
+		})
 
-	mobileBurgerBtn.addEventListener('click', () => {
-		mobileBurgerBtn.classList.toggle('navigation-btn--transform')
-		const choiceMenu = document.querySelector('.choice-menu')
-		choiceMenu.classList.toggle('choice-menu--show')
-	})
+		burgerBtn.addEventListener('click', () => {
+			burgerBtn.classList.toggle('navigation-btn--transform')
+			const choiceMenu = document.querySelector('.choice-menu')
+			choiceMenu.classList.toggle('choice-menu--show')
+
+			const eventsMenu = document.querySelector('.choice-menu--events')
+			if (eventsMenu.classList.contains('choice-menu--show')) {
+				eventsMenu.classList.remove('choice-menu--show')
+			}
+		})
+
+		mobileBurgerBtn.addEventListener('click', () => {
+			mobileBurgerBtn.classList.toggle('navigation-btn--transform')
+			const choiceMenu = document.querySelector('.choice-menu')
+			choiceMenu.classList.toggle('choice-menu--show')
+
+			const eventsMenu = document.querySelector('.choice-menu--events')
+			if (eventsMenu.classList.contains('choice-menu--show')) {
+				eventsMenu.classList.remove('choice-menu--show')
+			}
+		})
+	}
+
+	// меню событий
+	function eventsMenuActions() {
+		const eventMenus = [
+			document.getElementById('whoMenu'),
+			document.getElementById('whyMenu'),
+			document.getElementById('whatMenu'),
+		]
+
+		const eventsBtn = document.getElementById('eventsBtn')
+		const eventsMenu = document.querySelector('.choice-menu--events')
+
+		eventsBtn.addEventListener('click', () => {
+			eventsMenu.classList.toggle('choice-menu--show')
+
+			const mainMenu = document.querySelector('.choice-menu--main')
+			if (mainMenu.classList.contains('choice-menu--show')) {
+				mainMenu.classList.remove('choice-menu--show')
+				burgerBtn.classList.remove('navigation-btn--transform')
+				mobileBurgerBtn.classList.remove('navigation-btn--transform')
+			}
+		})
+
+		const whoChoice = document.getElementById('choiceWho')
+		const whyChoice = document.getElementById('choiceWhy')
+		const whatChoice = document.getElementById('choiceWhat')
+
+		// console.log(eventMenus, whoChoice, whyChoice, whatChoice)
+		whoChoice.addEventListener('click', () => {
+			showMenu(eventMenus, eventMenus[0])
+		})
+
+		whyChoice.addEventListener('click', () => {
+			showMenu(eventMenus, eventMenus[1])
+		})
+
+		whatChoice.addEventListener('click', () => {
+			showMenu(eventMenus, eventMenus[2])
+		})
+	}
+
+	burgerMenuActions()
+	eventsMenuActions()
 })
